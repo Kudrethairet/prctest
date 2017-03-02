033f25062d7cb80de3abd56be7bc7f102df8d78b
Kudret <kudrethairet@gmail.com>
2017-03-01 20:35:08 -0500

    SecondTesting

---
+++ Selenium/src/test/java/com/test/testing.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class testing {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is testing from other work space");
+		
+		
+		
+	}
+	
+	
+	
+}
