7e3d73dfb98ea76ec437b73f5ff0158b54c0dfb6
Kudret <kudrethairet@gmail.com>
2017-03-01 20:45:19 -0500

    third

--- Selenium/src/test/java/com/test/testing.java
+++ Selenium/src/test/java/com/test/testing.java
@@ -5,6 +5,7 @@ public class testing {
 	public static void main(String[] args) {
 		
 		System.out.println("This is testing from other work space");
+		System.out.println("this is testing for another one");
 		
 		
 		
